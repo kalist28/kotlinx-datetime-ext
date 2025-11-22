7aaeffa93c7a92c834462d014484ac86f1274db1
Dmitry <kalistratov.d.m@gmail.com>
2025-11-23 02:05:10 +0400

    initial commit

---
+++ kotlinx-datetime-ext/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.maven.publish)
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    androidTarget { publishLibraryVariants("release") }
+    jvm()
+    js { browser() }
+    wasmJs { browser() }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    linuxX64()
+    mingwX64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.datetime)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations["main"].compileTaskProvider.configure {
+            compilerOptions {
+                freeCompilerArgs.add("-Xexport-kdoc")
+            }
+        }
+    }
+
+}
+
+android {
+    namespace = "io.github.kalist28.datetime.ext"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+//Publishing your Kotlin Multiplatform library to Maven Central
+//https://www.jetbrains.com/help/kotlin-multiplatform-dev/multiplatform-publish-libraries.html
+mavenPublishing {
+    publishToMavenCentral()
+    coordinates("io.github.kalist28.datetime.ext", "kotlinx-datetime-ext", "1.0.0")
+
+    pom {
+        name = "kotlinx-datetime-ext"
+        description = "Kotlin Multiplatform library"
+        url = "github url" //todo
+
+        licenses {
+            license {
+                name = "MIT"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+
+        developers {
+            developer {
+                id = "" //todo
+                name = "" //todo
+                email = "" //todo
+            }
+        }
+
+        scm {
+            url = "github url" //todo
+        }
+    }
+    if (project.hasProperty("signing.keyId")) signAllPublications()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "kotlinx-datetime-ext"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":kotlinx-datetime-ext")
+include(":sample:composeApp")
+include(":sample:terminalApp")
+
